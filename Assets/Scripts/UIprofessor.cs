f4889f3f10a965a3aacec1eda49946fa660d2b71
Felipe Rosa <feliperibeirorosa@hotmail.com>
2024-08-22 21:06:20 -0300

    UI para professor

---
+++ Assets/Scripts/UIprofessor.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIprofessot : MonoBehaviour
+{
+
+    public enum Polygons {Cone, Cylinder};
+	public int polygon;
+
+    public float height;
+	public float width;
+    public int sides;
+    
+	static int sidesMinValue = 3;
+	static int sidesMaxValue = 20;
+
+	static float heightMinValue = 0.05;
+	static float heightMaxValue = 0.3;
+
+	static float widthMinValue = 0.05;
+	static float widthMaxValue = 0.3;
+
+	// Objetos para input
+	private GameObject eightSlider;
+	private GameObject widthSlider;
+	private GameObject sideSlider;
+
+	private TMP_Dropdown typeSelector;
+
+	// objetos para output
+	private TMP_Text heightText;
+	private TMP_Text widthText;
+	private TMP_Text sideText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+		// objetos de input
+        heightSlider = GameObject.Find("SliderHeight").GetComponent<Slider>();
+        heightSlider.minValue = heightMinValue;
+        heightSlider.maxValue = heightMaxValue;
+
+        heightSlider.onValueChanged.AddListener(HeightSliderUpdate);
+
+        widthSlider = GameObject.Find("SliderWidth").GetComponent<Slider>();
+        widthSlider.minValue = widthMinValue;
+        widthSlider.maxValue = widthMaxValue;
+
+        widthSlider.onValueChanged.AddListener(widthSliderUpdate);
+
+        sideSlider = GameObject.Find("SliderSide").GetComponent<Slider>();
+        sideSlider.minValue = sidesMinValue;
+        sideSlider.maxValue = sidestMaxValue;
+
+        sideSlider.onValueChanged.AddListener(sideSliderUpdate);
+
+		typeSelector = GameObject.Find("PolyDropdown").GetComponent<TMP_Dropdown>();
+		typeSelector.value = polygon;
+
+		typeSelector.onValueChanged.AddListener(delegate {typeUpdate(typeSelector);});
+
+		//output objetos de output
+		heightText = GameObject.Find("textHeight").GetComponent<TextMeshProUGUI>();
+		heightText.text = sides.toString();
+
+		widthText = GameObject.Find("textWidth").GetComponent<TextMeshProUGUI>();
+		widthText.text = sides.toString();
+
+		sideText = GameObject.Find("textSide").GetComponent<TextMeshProUGUI>();
+		sideText.text = sides.toString();
+    }
+}
