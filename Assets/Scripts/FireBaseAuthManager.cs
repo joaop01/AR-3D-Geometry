652f2966f9c54dc013d74765555506eac985ecf4
Igor Ribeiro <igor.ribeiro@unifesp.br>
2024-08-26 19:59:26 -0300

    Ambos botoes com redirecinamento para respectivas cenas

--- Assets/Scripts/FireBaseAuthManager.cs
+++ Assets/Scripts/FireBaseAuthManager.cs
@@ -6,15 +6,17 @@ using System.Threading.Tasks;
 using i5.Toolkit.Core.OpenIDConnectClient;
 using i5.Toolkit.Core.ServiceCore;
 using Firebase.Extensions;
+using UnityEngine.SceneManagement;
 
 
 public class FireBaseAuthManager : MonoBehaviour
 {
+    private string tipo_usuario="";
     // Start is called before the first frame update
     Firebase.Auth.FirebaseAuth auth;
     void Start()
     {
-         auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
     }
 
     // Update is called once per frame
@@ -23,7 +25,9 @@ public class FireBaseAuthManager : MonoBehaviour
         
     }
 
-    public void SignIn(){
+    public void SignIn(string Tipo_Usuario)
+    {
+        this.tipo_usuario = Tipo_Usuario;
         SignInWithGoogle();
     }
 
@@ -41,18 +45,35 @@ public class FireBaseAuthManager : MonoBehaviour
         Firebase.Auth.Credential credential = Firebase.Auth.GoogleAuthProvider.GetCredential(idToken:null, acessToken);
 
         auth.SignInAndRetrieveDataWithCredentialAsync(credential).ContinueWith(task => {
-        if (task.IsCanceled) {
-            Debug.LogError("SignInAndRetrieveDataWithCredentialAsync was canceled.");
-            return;
-        }
-        if (task.IsFaulted) {
-            Debug.LogError("SignInAndRetrieveDataWithCredentialAsync encountered an error: " + task.Exception);
-            return;
-        }
+            if (task.IsCanceled) {
+                Debug.LogError("SignInAndRetrieveDataWithCredentialAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted) {
+                Debug.LogError("SignInAndRetrieveDataWithCredentialAsync encountered an error: " + task.Exception);
+                return;
+            }
 
-      Firebase.Auth.AuthResult result = task.Result;
-      Debug.LogFormat("User signed in successfully: {0} ({1})",
-      result.User.DisplayName, result.User.UserId);
-    });
+            Firebase.Auth.AuthResult result = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                result.User.DisplayName, result.User.UserId);
+        });
+
+        if (tipo_usuario == "professor")
+        {
+            Debug.Log("Logado como professor.");
+            SwitchScenes("areaProfessor");
+
+        }
+        else if (tipo_usuario == "aluno")
+        {
+            Debug.Log("Logado como aluno.");
+            SwitchScenes("RenderPolygons");
+        }
     }
+    public void SwitchScenes(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
 }
